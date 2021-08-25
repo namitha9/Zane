18194fe0cb7a7b9ee8e53381c01507b878999592
namitha9 <namitha.justine@outlook.com>
2021-08-25 16:32:49 +0800

    Fifteenth Commit

--- lib/ProductList.dart
+++ lib/ProductList.dart
@@ -9,6 +9,12 @@ class Items{
   final String id;
 }
 
+class PandI{
+  const PandI(this.price, this.index);
+  final int price;
+  final int index;
+}
+
 class ProductList extends StatefulWidget {
   const ProductList({Key? key}) : super(key: key);
 
@@ -25,13 +31,137 @@ class _ProductListState extends State<ProductList> {
   String points = '';
   List <UserProfile> users =[UserProfile(id: '00', username: 'Name', email: 'Email', password: 'Password', blgno: '00', street: '000', zone: '00', points: '0', voucher: 'p')];
   List <int> prices = [];
-  List <int> dupe = [];
-  static List <int> indexOrder = [];
+  List <PandI> dupe = [];
+  static List <PandI> indexOrder = [];
+  //static List <int> indexOrder = [];
   List <Items> sortby = [
     const Items('Price Low to High', Icon(Icons.arrow_downward), '00'),
     const Items('Price High to Low', Icon(Icons.arrow_upward), '01')
   ];
   int pop=0;
+  int y = 0;
+
+  Widget display(catindex){
+    TotalItem instance = totalItems[catindex];
+    String price = instance.price.toString();
+    print(price);
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: TextButton(
+        onPressed: (){
+          Navigator.pushReplacementNamed(context, '/ProductDisplay',arguments: {
+            'picture': instance.picture ,
+            'price' : instance.price,
+            'description' : instance.description,
+            'points' : points,
+            'vouchers' : data['vouchers']
+          });
+        },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Image(
+              image: NetworkImage(instance.picture),
+              height: 175,
+              width: 175,
+            ),
+            SizedBox(
+                height: 10
+            ),
+            Text('QR $price',
+              style: TextStyle(color: Colors.black),),
+            Text(instance.description,
+              style: TextStyle(color: Colors.black),)
+          ],
+        ),
+      ),
+    );
+  }
+
+  void findIndex(){
+    indexOrder = [];
+    for (int l in prices){
+      pop = 0;
+      for (TotalItem instance in totalItems){
+        if(l==instance.price){
+          //indexOrder.add(pop);
+          break;
+        }
+        pop++;
+      }
+    }
+    print(indexOrder);
+  }
+
+  void select(String id) {
+    indexOrder.clear();
+    int p = 0;
+    for (TotalItem instance in totalItems){
+      setState(() {
+        indexOrder.add(PandI(instance.price, p));
+        p++;
+      });
+    }
+    if(id == '00'){
+      setState(() {
+        indexOrder.sort((a,b) => a.price.compareTo(b.price));
+      });
+    }
+    if(id == '01'){
+      setState(() {
+        indexOrder.sort((a,b) => a.price.compareTo(b.price));
+        dupe = List.from(indexOrder);
+        indexOrder = [];
+        indexOrder = List.from(dupe.reversed);
+      });
+    }
+    for(PandI instance in indexOrder){
+      print(instance.index);
+    }
+    //findIndex();
+  }
+
+  Widget displayInPage(List <PandI>indexOrder){
+    print('display');
+    print(indexOrder.elementAt(0).index);
+    return Padding(
+      padding: EdgeInsets.all(0),
+      child: Column(
+        children: [
+          Row(
+            children: [
+              display(indexOrder.elementAt(0).index),
+              display(indexOrder.elementAt(1).index),
+            ],
+          ),
+          Row(
+            children: [
+              display(indexOrder.elementAt(2).index),
+              display(indexOrder.elementAt(3).index),
+            ],
+          ),
+          Row(
+            children: [
+              display(indexOrder.elementAt(4).index),
+              display(indexOrder.elementAt(5).index),
+            ],
+          ),
+          Row(
+            children: [
+              display(indexOrder.elementAt(6).index),
+              display(indexOrder.elementAt(7).index),
+            ],
+          ),
+          Row(
+            children: [
+              display(indexOrder.elementAt(8).index),
+              display(indexOrder.elementAt(9).index),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -39,116 +169,10 @@ class _ProductListState extends State<ProductList> {
     totalItems = List.from(data['totalitems']);
     points = data['points'];
 
-    Widget display(catindex){
-      TotalItem instance = totalItems[catindex];
-      String price = instance.price.toString();
-      return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextButton(
-          onPressed: (){
-            Navigator.pushReplacementNamed(context, '/ProductDisplay',arguments: {
-              'picture': instance.picture ,
-              'price' : instance.price,
-              'description' : instance.description,
-              'points' : points,
-              'vouchers' : data['vouchers']
-            });
-          },
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Image(
-                image: NetworkImage(instance.picture),
-                height: 175,
-                width: 175,
-              ),
-              SizedBox(
-                  height: 10
-              ),
-              Text('QR $price',
-                style: TextStyle(color: Colors.black),),
-              Text(instance.description,
-                style: TextStyle(color: Colors.black),)
-            ],
-          ),
-        ),
-      );
+    if(y==0){
+      select('10');
+      y=1;
     }
-
-    void findIndex(){
-      indexOrder = [];
-      for (int l in prices){
-        pop = 0;
-        for (TotalItem instance in totalItems){
-          if(l==instance.price){
-            indexOrder.add(pop);
-            break;
-          }
-          pop++;
-        }
-      }
-      print(indexOrder);
-    }
-
-    void select(String id) {
-      prices = [];
-      for (TotalItem instance in totalItems){
-        prices.add(instance.price);
-      }
-      if(id == '00'){
-        prices.sort();
-      }
-      if(id == '01'){
-        prices.sort();
-        dupe = List.from(prices);
-        prices = [];
-        prices = List.from(dupe.reversed);
-      }
-      print(prices);
-      findIndex();
-    }
-
-    Widget displayInPage(){
-      return Padding(
-        padding: EdgeInsets.all(0),
-        child: Column(
-          children: [
-            Row(
-              children: [
-                display(indexOrder.elementAt(0)),
-                display(indexOrder.elementAt(1)),
-              ],
-            ),
-            Row(
-              children: [
-                display(indexOrder.elementAt(2)),
-                display(indexOrder.elementAt(3)),
-              ],
-            ),
-            Row(
-              children: [
-                display(indexOrder.elementAt(4)),
-                display(indexOrder.elementAt(5)),
-              ],
-            ),
-            Row(
-              children: [
-                display(indexOrder.elementAt(6)),
-                display(indexOrder.elementAt(7)),
-              ],
-            ),
-            Row(
-              children: [
-                display(indexOrder.elementAt(8)),
-                display(indexOrder.elementAt(9)),
-              ],
-            ),
-          ],
-        ),
-      );
-    }
-
-    select('10');
 
     return Scaffold(
       appBar: AppBar(
@@ -185,48 +209,55 @@ class _ProductListState extends State<ProductList> {
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               //crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                ElevatedButton.icon(
-                    onPressed: (){},
-                    icon: Icon(Icons.filter_alt_outlined),
-                  label: Text('FILTER',style: TextStyle(
-                      color: Colors.black
-                  ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.grey[500],
-                  ),
+                Row(
+                  children: [
+                    Text('FILTER'),
+                    PopupMenuButton(
+                        onSelected: select,
+                        icon: Icon(Icons.filter_alt_outlined),
+                        itemBuilder: (BuildContext context){
+                          return sortby.map((Items e){
+                            return PopupMenuItem<String>(
+                                value: e.id,
+                                child: Row(
+                                  children: [
+                                    e.icon,
+                                    SizedBox(width: 2,),
+                                    Text(e.name)
+                                  ],
+                                )
+                            );
+                          }).toList();
+                        }
+                    ),
+                  ],
                 ),
-                ElevatedButton.icon(
-                  onPressed: (){},
-                  icon: Icon(Icons.sort),
-                  label: Text('SORT BY',style: TextStyle(
-                      color: Colors.black
-                  ),
-                  ),
-                  style: ElevatedButton.styleFrom(
-                    primary: Colors.grey[500],
-                  ),
+                Row(
+                  children: [
+                    Text('SORT BY'),
+                    PopupMenuButton(
+                        onSelected: select,
+                        icon: Icon(Icons.sort),
+                        itemBuilder: (BuildContext context){
+                          return sortby.map((Items e){
+                            return PopupMenuItem<String>(
+                                value: e.id,
+                                child: Row(
+                                  children: [
+                                    e.icon,
+                                    SizedBox(width: 2,),
+                                    Text(e.name)
+                                  ],
+                                )
+                            );
+                          }).toList();
+                        }
+                    ),
+                  ],
                 ),
-                PopupMenuButton(
-                  onSelected: select,
-                    itemBuilder: (BuildContext context){
-                      return sortby.map((Items e){
-                        return PopupMenuItem<String>(
-                          value: e.id,
-                            child: Row(
-                              children: [
-                                e.icon,
-                                SizedBox(width: 2,),
-                                Text(e.name)
-                              ],
-                            )
-                        );
-                      }).toList();
-                    }
-                )
               ]
             ),
-            displayInPage(),
+            displayInPage(indexOrder),
           ],
         ),
       ),
